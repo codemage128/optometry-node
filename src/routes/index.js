cbfabacc5554ca4e460a9c2bcd4e67e26a9a2ad3
denisraleina <devsoftware907@gmail.com>
2021-01-31 22:23:17 -0500

    contact, about page design

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,13 @@ router.get('/', (req, res, next) => {
    res.render('index')
 })
 
+router.get('/about-us', async(req, res, next) => {
+   res.render('about-us');
+})
+
+router.get('/contact-us', async(req, res, next) => {
+   res.render('contact');
+})
+
+
 module.exports = router;
